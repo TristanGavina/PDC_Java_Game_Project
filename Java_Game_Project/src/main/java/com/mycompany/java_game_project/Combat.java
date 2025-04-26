ad206987d8bedd02d73703eba248ea60f86a16f8
Tristan Gavina <100380965+TristanGavina@users.noreply.github.com>
2025-04-26 23:15:27 +1200

    moved combat ui to GameUI class
    also made it serializable

--- Java_Game_Project/src/main/java/com/mycompany/java_game_project/Combat.java
+++ Java_Game_Project/src/main/java/com/mycompany/java_game_project/Combat.java
@@ -3,26 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.java_game_project;
-
+import java.io.*;
 /**
- * file i/o battle log
- *
+ * can do file i/o battle log
+ * This class handles combat logic
  * @author trist
  */
-import java.util.Scanner;
 
-public final class Combat {
+public final class Combat implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private boolean inCombat = false;
-    private final Scanner scan;
-    private final GameObjects player;
+    private transient UserInputs input;
+    private final Player player;
     private final Enemy currentEnemy;
+    private transient GameUI ui;
 
-    public Combat(GameObjects player, Enemy currentEnemy) {
-        scan = new Scanner(System.in);
+    public Combat(Player player, Enemy currentEnemy, UserInputs input, GameUI ui) {
+        this.input = input;
         this.player = player;
         this.currentEnemy = currentEnemy;
         this.inCombat = true;
+        this.ui = ui;
         startCombat();
 
     }
@@ -31,9 +33,9 @@ public final class Combat {
 
         while (inCombat) {
 
-            menu();
+            ui.combatMenu(player, currentEnemy);
             try {
-                int choice = Integer.parseInt(scan.nextLine().trim());
+                int choice = Integer.parseInt(input.getInput());
                 switch (choice) {
                     case 1: //attack
                         player.attack(currentEnemy);
@@ -66,44 +68,13 @@ public final class Combat {
     private void checkContinue() {
         if (currentEnemy.getHealth() <= 0) {
             inCombat = false;
-            menu();
+            ui.combatMenu(player, currentEnemy);
         }
     }
-
-    public void menu() {
-        System.out.print("""
-            _____________________________________________
-           |                                             |
-           |              CHOOSE ACTION!!!               |
-           |_____________________________________________|
-           |                                             |
-           |       Press [1] ATTACK                      |
-           |                                             |
-           |       Press [2] DEFEND                      |
-           |                                             |
-           |       Press [3] HEAL                        |
-           |                                             |
-           |       Press [4] RUN                         |
-           |_____________________________________________|
-           |=============================================|
-           """);
-        if (player.getHealth() <= 0) {
-            System.out.println("|       YOU HAVE BEEN KILLED!           |");
-        } else {
-            System.out.println(player.draw());
-        }
-
-        System.out.println("|_____________________________________________|");
-
-        if (currentEnemy.getHealth() <= 0) {
-            System.out.println("|   ENEMY: " + currentEnemy.type + " DEFEATED!");
-        } else {
-            System.out.println(currentEnemy.draw());
-        }
-
-        System.out.println("""
-                           |=============================================|
-                           |_____________________________________________|\n
-                           """);
+    
+    //Method for re-injecting transient dependencies after loading
+    public void injectDependencies(UserInputs input, GameUI ui){
+        this.input = input;
+        this.ui = ui;
     }
 }
