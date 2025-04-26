de44cb3f8275ba52b789e0245cd8be9da53d7b0c
Tristan Gavina <100380965+TristanGavina@users.noreply.github.com>
2025-04-26 23:27:42 +1200

    classes to handle user inputs for DIP

---
+++ Java_Game_Project/src/main/java/com/mycompany/java_game_project/UserInputProvider.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.mycompany.java_game_project;
+
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author trist
+ */
+public class UserInputProvider implements UserInputs, Serializable {
+        private static final long serialVersionUID = 1L;
+
+    private transient Scanner scan;
+    
+    public UserInputProvider(){
+        this.scan= new Scanner(System.in);
+    }
+    
+    @Override
+    public String getInput(){
+        if (scan == null) {
+            scan = new Scanner(System.in); // Reinitialize if deserialized
+        }
+        return scan.nextLine().trim();
+    }
+}
